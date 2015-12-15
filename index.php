2c95a7e722f54dda80b8e015a4423c8411cd2e19
Perdana Anwar <Perdana.Anwar@qsc.com>
2015-12-14 16:44:32 -0800

    added index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Git Hub Test</title>
+</head>
+
+<body>
+<ul>
+<li>Bread</li>
+<li>Turkey</li>
+<li>Cheese</li>
+</ul>
+</body>
+</html>
